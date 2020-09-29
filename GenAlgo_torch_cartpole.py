a1c4d115a107ac8d4a793068ce9e5496bfdba362
Elior Ben-Yosef <eliorby@gmail.com>
2020-09-30 02:52:43 +0300

    CHANGED clearer naming & docstring

--- GenAlgo_torch_cartpole.py
+++ GenAlgo_torch_cartpole.py
@@ -18,7 +18,7 @@ hidden_layers_units = [25, 10]  # The individual NN hidden layers
 
 gen_num = 25
 pop_size = 500
-mut_rate = 1e-2  # 1e-3
+mut_rate = 1e-2  # 1e-3  # the % of elements to mutate in each individual
 tournament_size = 0.2  # 20% of the population
 
 env_name = 'CartPole-v0'
@@ -29,7 +29,7 @@ n_actions = 2  # output layer
 
 # constant variables:
 
-params_key = 'params'
+params_vec_key = 'params_vec'
 fitness_key = 'fitness'
 
 layers_weights_shapes = []  # the parameter vector's layers (each layer's weight matrix dimension)
@@ -46,45 +46,41 @@ for i in range(len(hidden_layers_units) + 1):
 def init_population():
     """
     creates a population of agents (individuals),
-    each is a dict that stores its parameter vector & fitness score
+    each is a dict that stores its NN parameters vector & fitness score
     """
     population = []
     for i in range(pop_size):
-        vec = torch.randn(params_num) / 2.0
-        individual = {params_key: vec, fitness_key: 0}
-        population.append(individual)
+        population.append({params_vec_key: torch.randn(params_num) / 2.0, fitness_key: 0})
     return population
 
 
-def unpack_params(params):
+def split_params_vec(params_vec):
     """
-    unpacks or decomposes the 1-D (flat) parameter vector
-        into a set of individual-layer weight matrices & bias vectors stored in a list.
-    :param params: a flat parameter vector
-    :return: unpacked parameter vector
+    :param params_vec: NN parameters vector
+    :return: params_by_layer: a list of layer-wise tuples: (layer's weight matrix, layer's bias vector)
     """
-    unpacked_params = []
+    params_by_layer = []
     end_pt = 0
     for i, layer in enumerate(layers_weights_shapes):
         start_pt, end_pt = end_pt, end_pt + np.prod(layer)
-        weights = params[start_pt:end_pt].view(layer)
+        weights = params_vec[start_pt:end_pt].view(layer)
         start_pt, end_pt = end_pt, end_pt + layer[0]
-        bias = params[start_pt:end_pt]
-        unpacked_params.append((weights, bias))
-    return unpacked_params
+        bias = params_vec[start_pt:end_pt]
+        params_by_layer.append((weights, bias))
+    return params_by_layer
 
 
-def construct_model_and_pass_state(s, unpacked_params):
+def construct_model_and_pass_state(s, params_by_layer):
     """
-    constructs the NN model, and passed into it the input state
+    constructs the NN model, and passes the input state into it
     :param s: input state
-    :param unpacked_params: weight matrices & bias vectors of each layer [(w, b), ...]
+    :param params_by_layer: a list of layer-wise tuples: (layer's weight matrix, layer's bias vector)
     :return: probabilities over actions
     """
-    for i, unpacked_param in enumerate(unpacked_params):
-        w, b = unpacked_param
+    for i, layer_params in enumerate(params_by_layer):
+        w, b = layer_params
         x = torch.nn.functional.linear(s if i == 0 else x, w, b)  # logits
-        x = torch.relu(x) if i != len(unpacked_params) - 1 else torch.softmax(x, dim=0)  # probs
+        x = torch.relu(x) if i != len(params_by_layer) - 1 else torch.softmax(x, dim=0)  # probs
     return x
 
 
@@ -95,13 +91,13 @@ def calc_fitness(individual):
     (until it loses the game and returns the number of time steps it lasted as its fitness score).
     :return:
     """
-    unpacked_params = unpack_params(individual[params_key])
+    params_by_layer = split_params_vec(individual[params_vec_key])
 
     done = False
     fitness_score = 0
     s = torch.from_numpy(env.reset()).float()
     while not done:
-        probs = construct_model_and_pass_state(s, unpacked_params)
+        probs = construct_model_and_pass_state(s, params_by_layer)
         a = torch.distributions.Categorical(probs=probs).sample().item()
         s_, r, done, info = env.step(a)
         s = torch.from_numpy(s_).float()
@@ -130,14 +126,14 @@ def crossover(p1, p2):
     cross_pt = np.random.randint(params_num)
 
     o1_params = torch.zeros(params_num)
-    o1_params[0:cross_pt] = p1[params_key][:cross_pt]
-    o1_params[cross_pt:] = p2[params_key][cross_pt:]
-    o1 = {params_key: o1_params, fitness_key: 0.0}
+    o1_params[0:cross_pt] = p1[params_vec_key][:cross_pt]
+    o1_params[cross_pt:] = p2[params_vec_key][cross_pt:]
+    o1 = {params_vec_key: o1_params, fitness_key: 0.0}
 
     o2_params = torch.zeros(params_num)
-    o2_params[0:cross_pt] = p2[params_key][:cross_pt]
-    o2_params[cross_pt:] = p1[params_key][cross_pt:]
-    o2 = {params_key: o2_params, fitness_key: 0.0}
+    o2_params[0:cross_pt] = p2[params_vec_key][:cross_pt]
+    o2_params[cross_pt:] = p1[params_vec_key][cross_pt:]
+    o2 = {params_vec_key: o2_params, fitness_key: 0.0}
 
     return o1, o2
 
@@ -151,7 +147,7 @@ def mutate(individual):
     mut_num = int(mut_rate * params_num)  # number of elements to mutate
     mut_indices = np.random.randint(low=0, high=params_num, size=(mut_num,))
     # torch.randn() returns a tensor filled with random numbers from a standard normal distribution (mean 0, variance 1)
-    individual[params_key][mut_indices] = torch.randn(mut_num) / 10.0  # TODO: remove "/ 10.0" ?
+    individual[params_vec_key][mut_indices] = torch.randn(mut_num) / 10.0  # TODO: remove "/ 10.0" ?
     return individual
 
 
